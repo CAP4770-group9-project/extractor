56faaceb16a531401e50aaff3f623b3b6edc861a
Jonathan Rudich <rudich.jonathan@gmail.com>
2020-03-26 12:57:11 -0400

    added interval to API query

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   queries: require("./sp500Tickers.js").tickers,
   exactMatch: true,
   outputsize: "full", //full, compact
-  data_api: "TIME_SERIES_INTRADAY"
+  data_api: "TIME_SERIES_INTRADAY",
+  interval: "5min"
   //TIME_SERIES_INTRADAY, TIME_SERIES_DAILY, TIME_SERIES_DAILY_ADJUSTED, TIME_SERIES_WEEKLY, TIME_SERIES_WEEKLY_ADJUSTED, TIME_SERIES_MONTHLY, TIME_SERIES_MONTHLY_ADJUSTED
 };
