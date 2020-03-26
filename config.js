cd8acdc0f126de8a46c3cb62b560762af856709a
Jonathan Rudich <rudich.jonathan@gmail.com>
2020-03-26 12:45:23 -0400

    added stock tickers to config

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 module.exports = {
-    key: require("./key").key,
-    queries: [
-        "a",
-        "b"
-    ],
-    exactMatch: false,
-    outputsize: "full", //full, compact
-    data_api: "TIME_SERIES_DAILY"
-    //TIME_SERIES_INTRADAY, TIME_SERIES_DAILY, TIME_SERIES_DAILY_ADJUSTED, TIME_SERIES_WEEKLY, TIME_SERIES_WEEKLY_ADJUSTED, TIME_SERIES_MONTHLY, TIME_SERIES_MONTHLY_ADJUSTED
+  key: require("./key").key,
+  queries: require("./sp500Tickers.js").tickers,
+  exactMatch: true,
+  outputsize: "full", //full, compact
+  data_api: "TIME_SERIES_INTRADAY"
+  //TIME_SERIES_INTRADAY, TIME_SERIES_DAILY, TIME_SERIES_DAILY_ADJUSTED, TIME_SERIES_WEEKLY, TIME_SERIES_WEEKLY_ADJUSTED, TIME_SERIES_MONTHLY, TIME_SERIES_MONTHLY_ADJUSTED
 };
